f6d17816d1ac843d22c17f394bfd066bf502cbef
Kris Evans <kwjevans@gmail.com>
2012-08-01 22:47:34 +0100

    Initial Commit

---
+++ Arrays-2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Arrays-2
+//
+//  Created by Kris Evans on 01/08/2012.
+//  Copyright (c) 2012 Kris Evans. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
